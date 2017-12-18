2237da8cb55af05c9b813ae9b576deaaf3d71689
victor.govorov <victorgovorov@mail.ru>
2017-12-18 12:43:49 +0500

    add Byndyusoft.Extensions.Dapper.QueryObjects project

---
+++ src/Byndyusoft.Extensions.Dapper.QueryObjects/DbConnectionAsyncExtensions.cs
@@ -0,0 +1,85 @@
+﻿namespace Byndyusoft.Extensions.Dapper
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using global::Dapper;
+
+    public static class DbConnectionAsyncExtensions
+    {
+        public static Task<IEnumerable<T>> QueryAsync<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QueryAsync<T>(command);
+        }
+
+        public static Task<IEnumerable<dynamic>> QueryAsync(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QueryAsync(command);
+        }
+
+        public static Task<T> QueryFirstAsync<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QueryFirstAsync<T>(command);
+        }
+
+        public static Task<dynamic> QueryFirstAsync(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QueryFirstAsync(command);
+        }
+
+        public static Task<T> QueryFirstOrDefaultAsync<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QueryFirstOrDefaultAsync<T>(command);
+        }
+
+        public static Task<dynamic> QueryFirstOrDefaultAsync(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QueryFirstOrDefaultAsync(command);
+        }
+
+        public static Task<T> QuerySingleAsync<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QuerySingleAsync<T>(command);
+        }
+
+        public static Task<dynamic> QuerySingleAsync(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QuerySingleAsync(command);
+        }
+
+        public static Task<T> QuerySingleOrDefaultAsync<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QuerySingleOrDefaultAsync<T>(command);
+        }
+
+        public static Task<dynamic> QuerySingleOrDefaultAsync(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QuerySingleOrDefaultAsync(command);
+        }
+
+        public static Task<SqlMapper.GridReader> QueryMultipleAsync(this IDbConnection connection, QueryObject queryObject,
+            IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var command = CreateCommand(queryObject, transaction, commandTimeout, commandType, cancellationToken);
+            return connection.QueryMultipleAsync(command);
+        }
+
+        private static CommandDefinition CreateCommand(QueryObject queryObject, IDbTransaction transaction,
+            int? commandTimeout, CommandType? commandType, CancellationToken cancellationToken)
+        {
+            return new CommandDefinition(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout,
+                commandType, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ src/Byndyusoft.Extensions.Dapper.QueryObjects/DbConnectionExtensions.cs
@@ -0,0 +1,67 @@
+﻿namespace Byndyusoft.Extensions.Dapper
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using global::Dapper;
+
+
+    public static class DbConnectionExtensions
+    {
+        public static IEnumerable<T> Query<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.Query<T>(queryObject.Sql, queryObject.QueryParams, transaction, true, commandTimeout, commandType);
+        }
+
+        public static IEnumerable<dynamic> Query(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.Query(queryObject.Sql, queryObject.QueryParams, transaction, true, commandTimeout, commandType);
+        }
+
+        public static T QueryFirst<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QueryFirst<T>(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout, commandType);
+        }
+
+        public static dynamic QueryFirst(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QueryFirst(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout, commandType);
+        }
+
+        public static T QueryFirstOrDefault<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QueryFirstOrDefault<T>(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout, commandType);
+        }
+
+        public static dynamic QueryFirstOrDefault(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QueryFirstOrDefault(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout, commandType);
+        }
+
+        public static T QuerySingle<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QuerySingle<T>(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout, commandType);
+        }
+
+        public static dynamic QuerySingle(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QuerySingle(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout, commandType);
+        }
+
+        public static T QuerySingleOrDefault<T>(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QuerySingleOrDefault<T>(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout, commandType);
+        }
+
+        public static dynamic QuerySingleOrDefault(this IDbConnection connection, QueryObject queryObject, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QuerySingleOrDefault(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout, commandType);
+        }
+
+        public static SqlMapper.GridReader QueryMultiple(this IDbConnection connection, QueryObject queryObject,
+            IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QueryMultiple(queryObject.Sql, queryObject.QueryParams, transaction, commandTimeout,
+                commandType);
+        }
+    }
+}
